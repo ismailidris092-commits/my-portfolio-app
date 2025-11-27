dbcf6c764460c43bbb740591afdacd49921e2f50
Ismail Idris <ismailidris092@gmail.com>
2025-11-27 20:53:01 +0100

    updated portfolio

--- src/components/RecentWork.jsx
+++ src/components/RecentWork.jsx
@@ -3,9 +3,18 @@ import { Link } from "react-router-dom";
 
 export default function RecentWork() {
   return (
-    <div>
-      <Link to="/">Home</Link>
-      <h1>Recent Work</h1>
-    </div>
+    <>
+      <Link className="recent-work-link" to="/">
+        Home
+      </Link>
+      <div className="recent-work-container">
+        <h1>Recent Work</h1>
+        <p className="recent-work-para">
+          Solving user & business problems since last 15+ years. Lorem ipsum
+          dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+          incididunt ut labore et dolore magna aliqua.
+        </p>
+      </div>
+    </>
   );
 }
