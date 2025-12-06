2102fc0156165da7f01254c876662614da9a85cc
Ismail Idris <ismailidris092@gmail.com>
2025-12-06 17:17:50 +0100

    new updated portfolio

--- src/components/RecentWorks.jsx
+++ src/components/RecentWorks.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { useState } from "react";
 import vector from "../images/vector.png";
 import NavBar from "./NavBar";
 import Button from "./Button";
-import { Link } from "react-router-dom";
 import recentWorkSlides from "./recentWorkSlide";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export default function RecentWork() {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -29,73 +29,75 @@ export default function RecentWork() {
 
       <h1 className="recent-work-text">Recent Works</h1>
 
-      <Button nextSlide={nextSlide} prevSlide={prevSlide} />
+      <div className="pstn-relative-container">
+        <Button nextSlide={nextSlide} prevSlide={prevSlide} />
 
-      <section className="sunnyside-container">
-        <div className="sunnyside-section">
-          <img
-            className="sunnyside-img"
-            src={currentrecentWorkSlides.image1}
-            alt={currentrecentWorkSlides.image1}
-          />
-          <h2>{currentrecentWorkSlides.title1}</h2>
-          <p className="sunnyside-para">
-            {currentrecentWorkSlides.description1}
-          </p>
+        <section className="recentWork-container">
+          <div className="sunnyside-section">
+            <img
+              className="sunnyside-img"
+              src={currentrecentWorkSlides.image1}
+              alt={currentrecentWorkSlides.image1}
+            />
+            <h2>{currentrecentWorkSlides.title1}</h2>
+            <p className="sunnyside-para">
+              {currentrecentWorkSlides.description1}
+            </p>
 
-          <div className="view-code-web-btns">
-            <Link
-              to={currentrecentWorkSlides.codeLink1}
-              className="viewCode-btn"
-              target="_blank"
-            >
-              View Code
-              <img className="vector-icon" src={vector} alt="arrow icon" />
-            </Link>
+            <div className="view-code-web-btns">
+              <Link
+                to={currentrecentWorkSlides.codeLink1}
+                className="viewCode-btn"
+                target="_blank"
+              >
+                View Code
+                <img className="vector-icon" src={vector} alt="arrow icon" />
+              </Link>
 
-            <Link
-              to={currentrecentWorkSlides.webLink1}
-              className="viewWeb-btn"
-              target="_blank"
-            >
-              View Website
-              <img className="vector-icon" src={vector} alt="arrow icon" />
-            </Link>
+              <Link
+                to={currentrecentWorkSlides.webLink1}
+                className="viewWeb-btn"
+                target="_blank"
+              >
+                View Website
+                <img className="vector-icon" src={vector} alt="arrow icon" />
+              </Link>
+            </div>
           </div>
-        </div>
 
-        <div className="profitPredictor-section">
-          <img
-            className="profitPredictor-img"
-            src={currentrecentWorkSlides.image2}
-            alt={currentrecentWorkSlides.image2}
-          />
-          <h2>{currentrecentWorkSlides.title2}</h2>
-          <p className="profitPredictor-para">
-            {currentrecentWorkSlides.description2}
-          </p>
+          <div className="profitPredictor-section">
+            <img
+              className="profitPredictor-img"
+              src={currentrecentWorkSlides.image2}
+              alt={currentrecentWorkSlides.image2}
+            />
+            <h2>{currentrecentWorkSlides.title2}</h2>
+            <p className="profitPredictor-para">
+              {currentrecentWorkSlides.description2}
+            </p>
 
-          <div className="view-code-web-btns">
-            <Link
-              to={currentrecentWorkSlides.codeLink2}
-              className="viewCode-btn"
-              target="_blank"
-            >
-              View Code
-              <img className="vector-icon" src={vector} alt="arrow icon" />
-            </Link>
+            <div className="view-code-web-btns">
+              <Link
+                to={currentrecentWorkSlides.codeLink2}
+                className="viewCode-btn"
+                target="_blank"
+              >
+                View Code
+                <img className="vector-icon" src={vector} alt="arrow icon" />
+              </Link>
 
-            <Link
-              to={currentrecentWorkSlides.webLink2}
-              className="viewWeb-btn"
-              target="_blank"
-            >
-              View Website
-              <img className="vector-icon" src={vector} alt="arrow icon" />
-            </Link>
+              <Link
+                to={currentrecentWorkSlides.webLink2}
+                className="viewWeb-btn"
+                target="_blank"
+              >
+                View Website
+                <img className="vector-icon" src={vector} alt="arrow icon" />
+              </Link>
+            </div>
           </div>
-        </div>
-      </section>
+        </section>
+      </div>
     </>
   );
 }
